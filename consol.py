684fc2d94910d409bb0ce19ae9339633ecbbd2e4
Mohammed <mrcloud.med@gmail.com>
2024-03-09 22:34:03 +0100

    Console0.1

---
+++ consol.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+The Console Module
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """command interpreter class"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        print("Do not quit")
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
