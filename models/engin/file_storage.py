330d534f46d7034c968aa4efc888e78a638869f5
Mohammed <mrcloud.med@gmail.com>
2024-03-09 20:51:05 +0100

    FileStorage

--- models/engin/file_storage.py
+++ models/engin/file_storage.py
@@ -2,6 +2,9 @@
 """
 Module : File Storage
 """
+import json
+import os
+from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -29,11 +32,30 @@ class FileStorage:
         """
         serializes _objects to the JSON file
         """
-        
+        all_objs = FileStorage.__objects
+        obj_dict = {}
+        for obj in all_objs.keys():
+            obj_dict[obj] = all_objs[obj].to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
+
 
     def reload(self):
         """
         deserializes the JSON file to _objects
         Only if JSON file exists, otherwise, do nothing.
-        if the file doesn't exist, no exception should be raised"""
-        pass
+        if the file doesn't exist, no exception
+        should be raised
+        """
+        definedClasses = {'BaseModle': BaseModel}
+        
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                deserialized = json.load(file)
+                for obj_values in deserialized.values():
+                    clsName = obj_values["__class__"]
+                    cls_obj = definedClasses[clsName]
+                    self.new(cls_obj(**obj_values))
+        
+        except FileNotFoundError:
+            pass
