612abd98da03872a0f0ff10c7ebcff6c2181cc27
Mohammed <mrcloud.med@gmail.com>
2024-03-10 11:26:04 +0100

    update

--- console.py
+++ console.py
@@ -13,25 +13,25 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit command to exit the program
+        """
         """
         return True
+
+    def help_quit(self):
+        """
+        """
+        print("Quit command to exit the program\n")
 
     def do_EOF(self, arg):
         """Inbuilt EOF command to gracefully catch errors.
         """
-        print("")
+        print()
         return True
 
     def emptyline(self):
         """Do nothing when an empty line is entered
         """
         pass
-
-    def do_help(self, arg):
-        """To get help on a command, type help <topic>.
-        """
-        return super().do_help(arg)
 
 
 if __name__ == '__main__':
